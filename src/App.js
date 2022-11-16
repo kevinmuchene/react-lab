3b112f4165db73b5eb0c0a795779e1a6988d444a
Kevin Muchene <muchenekevin@gmail.com>
2022-11-16 08:29:27 -0600

    Commented out HooksLearn tag. Not to be included in the final commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Dashboard from "./pages/Dashboard";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <Dashboard />
+
+      {/* <HooksLearn /> */}
     </div>
   );
 }
