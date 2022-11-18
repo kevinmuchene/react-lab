4e4ae3f239faf0e7ed8348b7e01e634984c33ac1
Kevin Muchene <muchenekevin@gmail.com>
2022-11-18 10:07:12 -0600

    Used useContext instead of props to send the fetchId function

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -2,14 +2,12 @@ import React from "react";
 import Post from "../Post/Post";
 import "./posts.css";
 
-function Posts({ data, fetchPostIdWhenClicked }) {
-  // console.log(props);
+function Posts({ data }) {
   return (
     <div className="details">
       {data.map((post) => {
         return (
           <Post
-            fetchPostIdWhenClicked={fetchPostIdWhenClicked}
             id={post.id}
             title={post.title}
             author={post.author}
