6cd0828dcb3e1a9462a294d0bfffc14e9394adb4
Kevin Muchene <muchenekevin@gmail.com>
2022-11-18 09:59:59 -0600

    Using useContext to send post id

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { FetchPostContext } from "../../pages/Dashboard";
 
 function Post(props) {
+  const idFunctionContext = useContext(FetchPostContext);
   return (
     <div
       style={{ margin: "1.5em" }}
-      onClick={() => props.fetchPostIdWhenClicked(props.id)}
+      onClick={() => idFunctionContext(props.id)}
     >
       <p>Id : {props.id}</p>
       <p>Title: {props.title}</p>
